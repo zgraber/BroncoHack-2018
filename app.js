d7f672e540303b469c33c82b0d1411de852867d3
Alkennedy1998 <akennedy2@scu.edu>
2018-04-22 02:44:03 -0700

    all

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+var exphbs = require('express-handlebars');
+
 const app = express();
 const bodyParser = require('body-parser');
 const sqlMethods = require('./sqlMethods.js');
@@ -6,6 +8,30 @@ const path = require('path');
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./main.db');
+
+var hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+
+        grouped_each: function (every, context, options) {
+             var out = "", subcontext = [], i;
+             if (context && context.length > 0) {
+         for (i = 0; i < context.length; i++) {
+            if (i > 0 && i % every === 0) {
+                out += options.fn(subcontext);
+                subcontext = [];
+            }
+            subcontext.push(context[i]);
+        }
+        out += options.fn(subcontext);
+    }
+    return out;
+}
+    }
+});
+
+app.engine('handlebars',hbs.engine);
+app.set('view engine', 'handlebars');
 
 db.serialize(function() {
   db.run(sqlMethods.createTable);
@@ -17,8 +43,60 @@ db.close();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 const mentorsRouter = require('./controllers/mentors.js')
 app.use('/mentors', mentorsRouter);
+
+app.get('/', function (req, res) {
+
+    res.render('mainPage', {test: "John Smith!!!!"})
+});
+
+var data = [
+    {
+        "ID": 223,
+        "FNAME": "Colin",
+        "LNAME": "Savior",
+        "IMG": "./ayylmoa.jpg"
+    },
+    {
+        "ID": 22123,
+        "FNAME": "Cqwolin",
+        "LNAME": "Saqwvior",
+        "IMG": "./ayyqlmoa.jpg"
+    },
+    {
+        "ID": 2,
+        "FNAME": "mett",
+        "LNAME": "fukoni",
+        "IMG": "./a.jpg"
+    },
+    {
+        "ID": 2231,
+        "FNAME": "RE",
+        "LNAME": "s",
+        "IMG": "./fuck.jpg"
+    },
+    {
+    "ID": 2,
+    "FNAME": "William",
+    "LNAME": "Henrion",
+    "IMG": "./fuck.jpg"
+},
+{
+    "ID": 1234,
+    "FNAME": "William",
+    "LNAME": "Henrion",
+    "IMG": "./fuck.jpg"
+}
+
+]
+
+app.get('/mentorList', function (req, res) {
+
+    res.render('mentorList',{data: data});
+});
 
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
