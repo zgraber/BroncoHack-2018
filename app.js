88cbea8d5a593378c35cdec758b1375a470ca488
zgraber <zakgraber@gmail.com>
2018-04-21 20:31:45 -0700

    Added routes

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const sqlMethods = require('./sqlMethods.js');
+const path = require('path');
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./main.db');
 
 db.serialize(function() {
   db.run(sqlMethods.createTable);
+  //db.run(sqlMethods.newUser('1', 'Zak', 'Graber', 'Law', 'asdf', 'path', '1'));
 });
 
 db.close();
 
-ap.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const mentorsRouter = require('./controllers/mentors.js')
--- sqlMethods.js
+++ sqlMethods.js
@@ -1,12 +1,12 @@
 module.exports = {
 	//Makes the table initially
-	createTable: 'CREATE TABLE IF NOT EXISTS MENTORS(ID INT NOT NULL, FNAME VARCHAR(20) NOT NULL, LNAME VARCHAR(20) NOT NULL, CATEGORY VARCHAR(20) NOT NULL, DESCRIPTION VARCHAR(500),IMG VARCHAR(50), TIER INT, PRIMARY KEY (ID));'
+	createTable: 'CREATE TABLE IF NOT EXISTS MENTORS(ID INT NOT NULL, FNAME VARCHAR(20) NOT NULL, LNAME VARCHAR(20) NOT NULL, CATEGORY VARCHAR(20) NOT NULL, DESCRIPTION VARCHAR(500),IMG VARCHAR(50), TIER INT, PRIMARY KEY (ID));',
 	//Pulls all mentors from the table
-	allMentors: 'SELECT * FROM MENTORS;'
+	allMentors: 'SELECT * FROM MENTORS;',
 	//Select all the data on a user from the table
-	mentor: `SELECT $(ID) FROM MENTORS;`
+	mentor: function(id){return `SELECT * FROM MENTORS WHERE ID = ${id};`},
 	//Insert a new user into the table -- needs variable names as they are in VALUES(...)
-	newUser: 'INSERT INTO MENTORS(ID, FNAME, LNAME, CATEGORY, DESCRIPTION, IMG, TIER) VALUES($id, $fname, $lname, $category, $description, $img, $tier);'
+	newUser: function(id, fname, lname, category, description, img, tier) {return `INSERT INTO MENTORS(ID, FNAME, LNAME, CATEGORY, DESCRIPTION, IMG, TIER) VALUES(${id}, "${fname}", "${lname}", "${category}", "${description}", "${img}", ${tier});`},
 	//Update a user's values -- note that ALL values except ID need to be accounted for, even if they are unchanged
-	editUser: 'UPDATE MENTORS SET FNAME = $fname, LNAME = $lname, CATEGORY = $category, DESCRIPTION = $description, IMG = $img, TIER = $tier WHERE ID = $id;'	
+	editUser:function(fname, lname, category, description, img, tier, id){ return `UPDATE MENTORS SET FNAME = ${fname}, LNAME = ${lname}, CATEGORY = ${category}, DESCRIPTION = ${description}, IMG = ${img}, TIER = ${tier} WHERE ID = ${id};`}
 };
