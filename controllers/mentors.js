793845b0a420015250f874a0111ba20117bc70ce
zgraber <zakgraber@gmail.com>
2018-04-21 21:48:09 -0700

    Added a put route

--- controllers/mentors.js
+++ controllers/mentors.js
@@ -24,14 +24,14 @@ mentorsRouter.get('/:id', (req, res, next) => {
       console.log('User Not Found');
       res.status(404).send('User Not Found');
     } else {
-      res.send(row);
+      res.status(200).send(row);
     }
   });
 });
 
 mentorsRouter.post('/', (req, res, next) => {
   let body = req.body;
-  console.log(req.body);
+  //console.log(req.body);
   let id = body.id;
   let fname = body.fname;
   let lname = body.lname;
@@ -39,8 +39,22 @@ mentorsRouter.post('/', (req, res, next) => {
   let description = body.description;
   let img = body.img;
   let tier = body.tier;
-  console.log(sqlMethods.newUser(id, fname, lname, category, description, img, tier));
+  //console.log(sqlMethods.newUser(id, fname, lname, category, description, img, tier));
   db.run(sqlMethods.newUser(id, fname, lname, category, description, img, tier));
   console.log('User Added');
   res.status(201).redirect('/');
+});
+
+mentorsRouter.put('/:id', (req, res, next) => {
+  let body = req.body;
+  let id = req.params;
+  let fname = body.fname;
+  let lname = body.lname;
+  let category = body.category;
+  let description = body.description;
+  let img = body.img;
+  let tier = body.tier;
+  db.run(sqlMethods.editUser(fname, lname, category, description, img, tier, id));
+  console.log('User Edited');
+  res.status(200).send());
 });
