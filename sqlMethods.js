6c9a2669b9cba0fdf2e3f7c56542352938df61a3
zgraber <zakgraber@gmail.com>
2018-04-21 21:59:07 -0700

    idk

--- sqlMethods.js
+++ sqlMethods.js
@@ -8,5 +8,5 @@ module.exports = {
 	//Insert a new user into the table -- needs variable names as they are in VALUES(...)
 	newUser: function(id, fname, lname, category, description, img, tier) {return `INSERT INTO MENTORS(ID, FNAME, LNAME, CATEGORY, DESCRIPTION, IMG, TIER) VALUES(${id}, "${fname}", "${lname}", "${category}", "${description}", "${img}", ${tier});`},
 	//Update a user's values -- note that ALL values except ID need to be accounted for, even if they are unchanged
-	editUser:function(fname, lname, category, description, img, tier, id){ return `UPDATE MENTORS SET FNAME = ${fname}, LNAME = ${lname}, CATEGORY = ${category}, DESCRIPTION = ${description}, IMG = ${img}, TIER = ${tier} WHERE ID = ${id};`}
+	editUser:function(fname, lname, category, description, img, tier, id){ return `UPDATE MENTORS SET FNAME = ${fname}, LNAME = ${lname}, CATEGORY = ${category}, DESCRIPTION = ${description}, IMG = ${img}, TIER = ${tier} WHERE ID = ${id};`},
 };
